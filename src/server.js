1ed250379b2057dff911ccb1e6d1e49a9ecc5aae
Saurabh Kumar Jha <saurabh.jha.connect@gmail.com>
2026-08-02 16:49:04 +0530

    created first route

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-//Now this change happened inside develop branch
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.get('/health', (req, res) => {
+    res.send('Server is healthy!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
